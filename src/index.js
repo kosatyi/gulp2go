b04c72b2390a73db27ed6c134b3edaab0e45d5f0
Stepan Kosatyi <stepan@kosatyi.com>
2021-08-05 15:08:38 +0300

    update project

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
-const sass       = require('gulp-sass')(require('sass'));
-const svgSprite = require('gulp-svg-sprite');
-const touch = require('gulp-touch-fd');
-const cleanCSS = require('gulp-clean-css');
-const browserify = require('browserify');
-const vinylSource = require('vinyl-source-stream');
-const vinylBuffer = require('vinyl-buffer');
-const through2    = require('through2');
-const del    = require('del');
+import gulp from 'gulp';
+import sourcemaps from 'gulp-sourcemaps';
+import through2 from 'through2';
+import del from 'del';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import autoprefixer from 'gulp-autoprefixer';
+import svgSprite from 'gulp-svg-sprite';
+import touch from 'gulp-touch-fd';
+import cleanCSS from 'gulp-clean-css';
+import browserify from 'browserify';
+import vinylSource from 'vinyl-source-stream';
+import vinylBuffer from 'vinyl-buffer';
+import gulpSass from 'gulp-sass';
+import sassEngine from 'sass';
 
-module.exports['gulp'] = gulp;
-module.exports['del'] = del;
-module.exports['through2'] = through2;
-module.exports['sourcemaps'] = sourcemaps;
-module.exports['browserify'] = browserify;
-module.exports['vinylSource'] = vinylSource;
-module.exports['vinylBuffer'] = vinylBuffer;
-module.exports['svgSprite'] = svgSprite;
-module.exports['uglify'] = uglify;
-module.exports['rename'] = rename;
-module.exports['touch'] = touch;
-module.exports['sass'] = sass;
+const sass = gulpSass(sassEngine);
 
-const svgBundler  = function(files,bundle,target){
-    return gulp.src(files)
+const extend = (defaults, options) => Object.assign({}, defaults, options);
+
+export {
+    gulp,
+    del,
+    through2,
+    sourcemaps,
+    sass,
+    browserify,
+    vinylSource,
+    vinylBuffer,
+    svgSprite,
+    uglify,
+    rename,
+    touch
+}
+
+export const svgBundler = (files, bundle, target) => {
+    return () => gulp.src(files)
         .pipe(svgSprite({
-            mode: { stack: { sprite: bundle } }
+            mode: {stack: {sprite: bundle}}
         }))
-        .on('error', function(err){
+        .on('error', function (err) {
             console.log(err);
             this.emit('end');
         })
@@ -40,76 +47,73 @@ const svgBundler  = function(files,bundle,target){
         .pipe(touch());
 }
 
-module.exports['svgBundler'] = svgBundler;
 
-const scssBundler = function(files,target){
-    return gulp.src(files)
+export const scssBundler = (files, target) => {
+    return () => gulp.src(files)
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error',sass.logError))
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(gulp.dest(target))
         .pipe(cleanCSS())
-        .pipe(rename({extname:'.min.css'}))
+        .pipe(rename({extname: '.min.css'}))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(target))
         .pipe(touch());
 }
 
-module.exports['scssBundler'] = scssBundler;
+const babelifyDefaults = {
+    "plugins": [
+        ["@babel/plugin-transform-typescript", {
+            "allExtensions": true,
+            "allowDeclareFields": true
+        }],
+        ["@babel/plugin-proposal-class-properties"],
+        ["@babel/plugin-transform-runtime"]
+    ],
+    "presets": [
+        ["@babel/preset-env", {}]
+    ],
+    "sourceMaps": true
+}
 
-const jsBundler = function(source,bundle,target,settings){
-
-    const params  = {
-        plugins:[],
-        transform: [],
-        cache: {},
-        packageCache: {}
+export const jsBundler = (source, bundle, target, settings) => {
+    const plugins = [];
+    const transform = [];
+    const params = {
+        standalone: false,
+        debug: true,
+        plugins,
+        transform
     };
-
-    params.debug = true;
-
-    params.transform.push('sourceify');
-
-    if( 'tsify' in settings ){
-        params.plugins.push( ['tsify',settings['tsify']]);
+    transform.push(['sourceify']);
+    if ('tsify' in settings) {
+        plugins.push(['tsify', settings['tsify']]);
     }
-    if( 'watchify' in settings ){
-        params.plugins.push(['watchify'])
+    if ('babelify' in settings) {
+        transform.push(['babelify', extend(babelifyDefaults, settings['babelify'])]);
     }
-    if( 'babelify' in settings ){
-        params.transform.push(['babelify',settings['babelify']]);
+    if ('schemify' in settings) {
+        transform.push(['./src/schemify', settings['schemify']]);
     }
-    if( 'schemify' in settings ){
-        params.transform.push(['./src/schemify',settings['schemify']]);
+    if ('standalone' in settings) {
+        params.standalone = settings.standalone;
     }
-    if( 'standalone' in settings ){
-        params.standalone =  settings.standalone;
-    }
-
-    const build = browserify(source,params);
-
-    const callback = function(){
-        return build.bundle()
-            .pipe(vinylSource(bundle))
-            .pipe(vinylBuffer())
-            .pipe(sourcemaps.init({loadMaps: true}))
-            .pipe(gulp.dest(target))
-            .pipe(uglify())
-            .pipe(rename({
-                extname:'.min.js'
-            }))
-            .pipe(sourcemaps.write('./'))
-            .pipe(gulp.dest(target))
-            .pipe(touch());
-    }
-
-    build.on('update', callback);
-
-    return callback();
+    return () => browserify(source, params).bundle()
+        .pipe(vinylSource(bundle))
+        .pipe(vinylBuffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(gulp.dest(target))
+        .pipe(uglify())
+        .pipe(rename({
+            extname: '.min.js'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(target))
+        .pipe(touch());
 
 }
 
-module.exports['jsBundler'] = jsBundler;
+
 
 
 
