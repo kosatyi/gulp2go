c3aaf8f27593becb00faa994607277b53f0b34cd
Stepan Kosatyi <stepan@kosatyi.com>
2022-12-06 20:49:16 +0200

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const through2 = require('through2');
-const del = require('del');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
-const svgSprite = require('gulp-svg-sprite');
-const touch = require('gulp-touch-fd');
-const cleanCSS = require('gulp-clean-css');
-const browserify = require('browserify');
-const vinylSource = require('vinyl-source-stream');
-const vinylBuffer = require('vinyl-buffer');
-const purify = require('gulp-purify-css');
-const concat = require('gulp-concat');
-const tools = require('browserify-transform-tools');
-const sass = require('gulp-sass')(require('sass'));
-
+import gulp from 'gulp'
+import sourcemaps from 'gulp-sourcemaps'
+import through2 from 'through2'
+import del from 'del'
+import uglify from 'gulp-uglify'
+import rename from 'gulp-rename'
+import autoprefixer from 'gulp-autoprefixer'
+import svgSprite from 'gulp-svg-sprite'
+import touch from 'gulp-touch-fd'
+import cleanCSS from 'gulp-clean-css'
+import browserify from 'browserify'
+import vinylSource from 'vinyl-source-stream'
+import vinylBuffer from 'vinyl-buffer'
+import purify from 'gulp-purify-css'
+import concat from 'gulp-concat'
+import sass from 'gulp-sass'
+import { makeRequireTransform} from 'browserify-transform-tools'
 const extend = (...sources) => Object.assign({}, ...sources);
 
 const wrapper = p => `(typeof window !== "undefined" ? window['${p}'] : typeof global !== "undefined" ? global['${p}'] : null)`;
 
-const schemify = tools.makeRequireTransform('schemify',{
+const schemify = makeRequireTransform('schemify', {
     evaluateArguments: true,
     jsFilesOnly: true
-},(args, opts, cb) => {
+}, (args, opts, cb) => {
     const shimmedModules = opts.config || {};
     const moduleName = args[0];
     const shim = shimmedModules[moduleName];
-    if(typeof shim === 'undefined') {
+    if (typeof shim === 'undefined') {
         return cb();
     } else {
         return cb(null, wrapper(shim));
@@ -41,9 +40,9 @@ const schemify = tools.makeRequireTransform('schemify',{
  * @return {*}
  */
 const svgBundler = (files, bundle, target) => {
-    return gulp.src(files)
+    return src(files)
         .pipe(svgSprite({
-            mode: {stack:{sprite: bundle}}
+            mode: {stack: {sprite: bundle}}
         }))
         .on('error', function (err) {
             console.log(err);
@@ -66,14 +65,14 @@ const scssBundler = (files, target, settings = {}) => {
     let chain = gulp.src(files);
     chain = chain.pipe(sourcemaps.init({}));
     chain = chain.pipe(sass().on('error', sass.logError));
-    if('purify' in settings){
+    if ('purify' in settings) {
         chain = chain.pipe(purify(settings['purify']));
     }
     return chain.pipe(autoprefixer(settings['autoprefixer'] || {}))
         .pipe(gulp.dest(target))
         .pipe(cleanCSS(settings['clean'] || {}))
         .pipe(rename({extname: '.min.css'}))
-        .pipe(sourcemaps.write('./',{}))
+        .pipe(sourcemaps.write('./', {}))
         .pipe(gulp.dest(target))
         .pipe(touch());
 };
@@ -133,26 +132,30 @@ const jsBundler = (source, bundle, target, settings = {}) => {
         .pipe(rename({
             extname: '.min.js'
         }))
-        .pipe(sourcemaps.write('./',{}))
+        .pipe(sourcemaps.write('./', {}))
         .pipe(gulp.dest(target))
         .pipe(touch());
 };
 
-exports.gulp = gulp;
-exports.through2 = through2;
-exports.sourcemaps = sourcemaps;
-exports.sass = sass;
-exports.del = del;
-exports.browserify = browserify;
-exports.vinylSource = vinylSource;
-exports.vinylBuffer = vinylBuffer;
-exports.svgSprite = svgSprite;
-exports.uglify = uglify;
-exports.rename = rename;
-exports.touch = touch;
-exports.purify = purify;
-exports.concat = concat;
 
-exports.jsBundler = jsBundler;
-exports.scssBundler = scssBundler;
-exports.svgBundler = svgBundler;
+export {
+    gulp,
+    through2,
+    sourcemaps,
+    del,
+    sass,
+    browserify,
+    vinylSource,
+    vinylBuffer,
+    svgSprite,
+    uglify,
+    rename,
+    touch,
+    purify,
+    concat,
+    jsBundler,
+    scssBundler,
+    svgBundler
+}
+
+
